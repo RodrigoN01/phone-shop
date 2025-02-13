bd66a7a6238476a5e4b16bd85b7a0362aefb7b26
Rodrigo <rod_novaes@hotmail.com>
2025-02-13 21:34:57 +0100

    Update next config file to support images from remote sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "prueba-tecnica-api-tienda-moviles.onrender.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
