68fbe8faeb4d4cd783ef1ce8b0bc16a975b34a0a
Rodrigo <rod_novaes@hotmail.com>
2025-02-16 18:50:10 +0100

    Update PhoneList component to use the new PhoneCard

--- src/components/PhonesList/PhonesList.tsx
+++ src/components/PhonesList/PhonesList.tsx
@@ -1,7 +1,7 @@
 import { Phone } from "@/types";
 import Link from "next/link";
-import Image from "next/image";
 import styles from "./PhonesList.module.scss";
+import PhoneCard from "../PhoneCard/PhoneCard";
 
 interface PhonesListProps {
   phones: Phone[];
@@ -12,21 +12,12 @@ export default function PhonesList({ phones }: PhonesListProps) {
     <ul className={styles.PhonesList}>
       {phones.map((phone, index) => (
         <Link href={`/phones/${phone.id}`} key={`${phone.id}-${index}`}>
-          <li className={styles.PhonesList__card}>
-            <Image
-              className={styles.PhonesList__card__image}
-              src={phone.imageUrl}
-              alt={phone.name}
-              width={220}
-              height={220}
-              priority
-            />
-            <p className={styles.PhonesList__card__brand}>{phone.brand}</p>
-            <div className={styles.PhonesList__card__info}>
-              <p>{phone.name}</p>
-              <p>${phone.basePrice}</p>
-            </div>
-          </li>
+          <PhoneCard
+            name={phone.name}
+            price={phone.basePrice}
+            image={phone.imageUrl}
+            brand={phone.brand}
+          />
         </Link>
       ))}
     </ul>
